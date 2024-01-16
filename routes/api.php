7fb294ea1eea5ecc55ae7ec21fc014eb0d0eae1e
hicham <hichamghoutani52@gmail.com>
2024-01-16 15:59:10 +0100

    email smtp configuration

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,20 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\MailController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/forget-password', [AuthController::class, 'forgetPassword']);
 
+Route::post('/resetPassword', [MailController::class, 'send']);
+Route::get('/resetPassword', [MailController::class, 'send']);
 
 Route::group(['middleware' => 'api'], function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    
         
 });
 
